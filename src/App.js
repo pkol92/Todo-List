9698d948e57b710ef74941ec60b8e81abb2638d9
pkol92 <paulina_kolasa@wp.pl>
2022-01-20 21:07:46 +0100

    add new code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <TodoForm />
+      <TodoList/>
     </div>
   );
 }
--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -1,11 +1,26 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-function TodoForm() {
+function TodoForm(props) {
     const [input, setInput] = useState('');
+
+    const inputRef = useRef(null);
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random()*1000),
+            text: input
+        })
+       setInput('');
     };
 
     return (
@@ -15,7 +30,9 @@ function TodoForm() {
             placeholder='Add a todo'
             value={input}
             name='text'
-            className='todo-input'/>
+            className='todo-input'
+            onChange={handleChange}
+            ref={inputRef}/>
             <button className='todo-button'>Add todo</button>
         </form>
     )
